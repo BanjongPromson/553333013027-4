dcc11b23386188f62cd73e938b295e2e15b37e67
BanjongPromson <promson_yoo@hotmail.com>
2014-04-09 15:33:57 +0700

    Test

---
+++ src/test/Test.java
@@ -0,0 +1,10 @@
+package test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Banjong Promson\n553333013027-4\nECP3R");
+	}
+
+}
