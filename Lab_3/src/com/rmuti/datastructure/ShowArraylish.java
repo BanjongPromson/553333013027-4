0887831fa29658a74bc9d067d77d5c0e01308dba
BanjongPromson <promson_yoo@hotmail.com>
2014-04-27 13:52:11 +0700

    test

--- Lab_3/src/com/rmuti/datastructure/ShowArraylish.java
+++ Lab_3/src/com/rmuti/datastructure/ShowArraylish.java
@@ -3,12 +3,49 @@ package com.rmuti.datastructure;
 public class ShowArraylish {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		ArrayList lst = new ArrayList(); 
-		 lst.add("a"); 
-		 lst.add("b"); 
-		 System.out.println(lst);
-
+		
+/****************************** Show Arraylish ******************************/
+		ArrayList lst = new ArrayList();
+		System.out.println(">>>>> Show Arraylish <<<<<");
+		lst.add("Y");
+		lst.add("o");
+		lst.add("o");
+		System.out.println(lst);
+		System.out.println("");
+		
+		
+/****************************** Show Stack ******************************/
+		ArrayListStack stk = new ArrayListStack();
+		System.out.println(">>>>>>> Show Stack <<<<<<<");
+		stk.push('A');
+		stk.push('B');
+		stk.push('C');
+		stk.push('D');
+		stk.push('E');
+		stk.Show_Stack();
+		System.out.println("pop = " + stk.pop());
+		stk.Show_Stack();
+		System.out.println("");
+		
+		
+/****************************** Show Queue ******************************/
+		ArrayListQueue q = new ArrayListQueue();
+		System.out.println(">>>>>>> Show Queue <<<<<<<");
+		q.enQueue("A");
+		q.enQueue("B");
+		q.enQueue("C");
+		q.enQueue("D");
+		q.enQueue("F");
+		q.enQueue("G");
+		q.Show_Queue();
+		System.out.println("De Queue = " + q.deQueue());
+		q.Show_Queue();
+		System.out.println("De Queue = " + q.deQueue());
+		q.Show_Queue();
+		System.out.println("De Queue = " + q.deQueue());
+		q.Show_Queue();
+		System.out.println("First Data = " + q.peek());
+		System.out.println("Data in Queue = " + q.getSize());
 	}
 
 }
