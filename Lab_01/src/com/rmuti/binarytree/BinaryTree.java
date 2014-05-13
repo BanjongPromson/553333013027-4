3720ee560de27ae0d92c1bb1cb94a3642b7887ee
BANJONG <BANJONG@192.168.30.64>
2014-05-13 17:20:42 +0700

    binary tree

--- Lab_01/src/com/rmuti/binarytree/BinaryTree.java
+++ Lab_01/src/com/rmuti/binarytree/BinaryTree.java
@@ -1,94 +1,167 @@
 package com.rmuti.binarytree;
 
+
 public class BinaryTree {
-	static class Node {
-		Object element;
-		Node left, right;
 
-		Node(Object e, Node l, Node r) {
-			element = e;
-			left = l;
-			right = r;
-		}
-		public boolean isLeaf() {
-			return left == null && right == null;
-		}
-	}
-	Node root;
-	public int numberNodes() {
-		return numNodes(root);
-	}
-	public int numNodes(Node node) {
-		if (node == null)
-			return 0;
-		return 1 + numNodes(node.left) + numNodes(node.right);
-	}
+    TreeNode root;
+    TreeNode newNode;
+    TreeNode parent;
+    TreeNode NodeDelete;
+    char child;
+    int countLevel;
+    //เมธอดตรวจสอบว่าทรีว่างเปล่าหรือไม่    
 
-	public static void main(String[] args) {
-		BinaryTree binaryTree = new BinaryTree();
-		Node right3 = new Node("f",null,null);
-		Node right2 = new Node("e",null,null);
-		Node left2 = new Node("d", null, null);
-		Node left1 = new Node("b", left2, right2);
-		Node right1 = new Node("c",null, right3);
-		Node root = new Node("a", left1, right1);
-		binaryTree.root = root;
-		System.out.println(binaryTree.numberNodes());
-	}
+    public boolean isEmpty() {
+        return root == null;
+    }
+    //เมธอดค้นหาตำแหน่งในการเพิ่มโหนดในไบนารีทรี        
 
-	public abstract class Visitor {
-		public abstract void visit(Object e);
+    public void insertItem(TreeNode bst, int newItem) {
+        if (newItem < bst.item) {
+            if (bst.lChild == null) {
+                newNode = new TreeNode(newItem);
+                bst.lChild = newNode;
+            } else {
+                insertItem(bst.lChild, newItem);
+            }
+        } else {
+            if (bst.rChild == null) {
+                newNode = new TreeNode(newItem);
+                bst.rChild = newNode;
+            } else {
+                insertItem(bst.rChild, newItem);
+            }
+        }
+    }
+    //เพิ่มข้อมูลในไบนารีทรี  
 
-		void preOrder(Node x) {
-			if (x == null) 
-				return;
-				visit(x.element);
-				preOrder(x.left);
-				preOrder(x.right);
+    public TreeNode insert(int newItem) {
+        if (isEmpty()) {
+            newNode = new TreeNode(newItem);
+            root = newNode;
+        } else {
+            insertItem(root, newItem);
+        }
+        return root;
+    }
+    //ค้นหาโหนดในไบนารีทรีเพื่อทำการลบโหนด
 
-		}
+    public void search(TreeNode bst, int searchKey) {
+        if (searchKey == bst.item) {
+            NodeDelete = bst;
+        } else if (searchKey < bst.item) {
+            parent = bst;
+            child = 'l';
+            search(bst.lChild, searchKey);
+        } else {
+            parent = bst;
+            child = 'r';
+            search(bst.rChild, searchKey);
+        }
+    }
+//ค้นหาโหนดด้วยหลักการ inordersuccessor
+    public void inordersuccessor(TreeNode bst) {
+        if (bst.lChild != null) {
+            parent = bst;
+            ++countLevel;
+            inordersuccessor(bst.lChild);
+        } else {
+            NodeDelete.item = bst.item;
+            if (countLevel == 0) {
+                NodeDelete.rChild = null;
+            } else {
+                parent.lChild = null;
+            }
+        }
+    }
+    //ลบโหนดในไบนารีทรี   
 
-		void inOrder(Node x) {
-			if (x == null) 
-				return;
-				inOrder(x.left);
-				visit(x.element);
-				inOrder(x.right);
+    public void delete(int deleteItem) {
+        if (isEmpty()) {
+            System.out.println("Tree Empty");
+        } else {
+            parent = root;
+            NodeDelete = null;
+            search(root, deleteItem);
+            TreeNode lChild = NodeDelete.lChild;
+            TreeNode rChild = NodeDelete.rChild;
+            if ((lChild == null) && (rChild == null)) {
+                //โหนดที่ต้องการลบอยู่ตำแหน่งใบ
+                if (child == 'l') {
+                    parent.lChild = null;
+                } else {
+                    parent.rChild = null;
+                }
+            } else if ((lChild != null) && (rChild != null)) {
+                //โหนดที่ต้องการลบมีโหนดลูกสองโหนด
+                countLevel = 0;
+                inordersuccessor(NodeDelete.rChild);
+            } else {
+                //โหนดที่ต้องการลบมีโหนดลูกหนึ่งโหนด 
+                if (NodeDelete.lChild != null) {
+                    if (child == 'l') {
+                        parent.lChild = NodeDelete.lChild;
+                    } else {
+                        parent.rChild = NodeDelete.lChild;
+                    }
+                } else {
+                    if (child == 'l') {
+                        parent.lChild = NodeDelete.rChild;
+                    } else {
+                        parent.rChild = NodeDelete.rChild;
+                    }
+                }
+            }
+        }
+    }
+    //ท่องเข้าไปในไบนารีทรีด้วยหลักการ preOrder   
 
-		}
+    public void preOrder(TreeNode rootNode) {
+        if (rootNode != null) {
+            System.out.print(rootNode.item + " ");
+            preOrder(rootNode.lChild);
+            preOrder(rootNode.rChild);
+        }
+    }
+    //ท่องเข้าไปในไบนารีทรีด้วยหลักการ inOrder   
 
-		void postOrder(Node x) {
-			if (x == null) 
-				return;
-				postOrder(x.left);
-				postOrder(x.right);
-				visit(x.element);
+    public void inOrder(TreeNode rootNode) {
+        if (rootNode != null) {
+            inOrder(rootNode.lChild);
+            System.out.print(rootNode.item + " ");
+            inOrder(rootNode.rChild);
+        }
+    }
+    //ท่องเข้าไปในไบนารีทรีด้วยหลักการ postOrder  
 
-		}
-	}
+    public void postOrder(TreeNode rootNode) {
+        if (rootNode != null) {
+            postOrder(rootNode.lChild);
+            postOrder(rootNode.rChild);
+            System.out.print(rootNode.item + " ");
+        }
+    }
 
-	@SuppressWarnings("null")
-	void preOrder(Node r, Visitor v) {
-		if (r == null) {
-			v.visit(r.element);
-			preOrder(r.left, v);
-			preOrder(r.right, v);
-		}
-	}
-
-	public Object[] toArray() {
-		final Object[] a = new Object[numberNodes()];
-		Visitor v = new Visitor() {
-			int k = 0;
-
-			@Override
-			public void visit(Object e) {
-				a[k++] = e;
-
-			}
-		};
-		preOrder(root, v);
-		return a;
-	}
-
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree();
+        TreeNode root = new TreeNode();
+        root = tree.insert(60);
+        tree.insert(20);
+        tree.insert(10);  
+        tree.insert(40);
+        tree.insert(30);
+        tree.insert(50);
+        tree.insert(70);
+        System.out.println("Insert: 60 20 10 40 30 50 70 ");
+        System.out.print("Preorder: ");
+        tree.preOrder(root);
+        tree.delete(20);
+        System.out.println("\nDelete: 20");
+        System.out.print("Preorder: ");
+        tree.preOrder(root);
+        System.out.print("\nInorder: ");
+        tree.inOrder(root);
+        System.out.print("\nPostorder: ");
+        tree.postOrder(root);
+    }
 }
---
+++ Lab_01/src/com/rmuti/binarytree/TreeNode.java
@@ -0,0 +1,24 @@
+package com.rmuti.binarytree;
+
+public class TreeNode {
+
+    protected int item;
+    protected TreeNode lChild;
+    protected TreeNode rChild;
+
+    TreeNode() {
+    }
+
+    TreeNode(int newItem) {
+        item = newItem;
+        lChild = null;
+        rChild = null;
+    }
+
+    public TreeNode(final int newItem, final TreeNode newlChild, final TreeNode newrChild) {
+        item = newItem;
+        lChild = newlChild;
+        rChild = newrChild;
+    }
+}
+
