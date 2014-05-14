e30ece2175f9fdc055517ce508913278bbd284bd
BANJONG <BANJONG@YOO.connectify>
2014-05-14 18:14:27 +0700

    Lab

---
+++ Lab/src/com/rmuti/datastructure/ShowArrayList.java
@@ -0,0 +1,44 @@
+package com.rmuti.datastructure;
+
+public class ShowArrayList {
+	public static void main(String[] args) {
+
+		ArrayList lst = new ArrayList();
+		lst.add("Y");
+		lst.add("O");
+		lst.add("O");
+		lst.add("0");
+		lst.add("2");
+		lst.add("7");
+		System.out.println(lst);
+		System.out.println("\n");
+		
+		ArrayListStack stk = new ArrayListStack();
+		stk.push('A');
+		stk.push('B');
+		stk.push('C');
+		stk.push('D');
+		stk.push('E');
+		stk.Show_Stack();
+		System.out.println("pop = " + stk.pop());
+		stk.Show_Stack();
+		System.out.println("\n");
+
+		ArrayListQueue q = new ArrayListQueue();
+		q.enQueue("A");
+		q.enQueue("B");
+		q.enQueue("C");
+		q.enQueue("D");
+		q.enQueue("F");
+		q.enQueue("G");
+		q.Show_Queue();
+		System.out.println("De Queue = " + q.deQueue());
+		q.Show_Queue();
+		System.out.println("De Queue = " + q.deQueue());
+		q.Show_Queue();
+		System.out.println("De Queue = " + q.deQueue());
+		q.Show_Queue();
+		System.out.println("First Data = " + q.peek());
+		System.out.println("Data in Queue = " + q.getSize());
+	}
+}
---
+++ Lab/src/com/rmuti/priorityqueue/ArrayLisyPQ.java
@@ -0,0 +1,30 @@
+package com.rmuti.priorityqueue;
+
+import com.rmuti.datastructure.ArrayList;
+
+
+public class ArrayLisyPQ {
+	ArrayList lst = new ArrayList();
+	public void enQueue(Object e) {
+		lst.add(e);
+	}
+	public Object deQueue() {	
+		Object a = lst.get(maxIndex());
+		lst.remove(maxIndex());
+		return a;
+	}
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	private int maxIndex() {
+	       int max = 0;
+	       for (int i = 0; i < lst.getSize(); i++) {
+	                 Comparable d = (Comparable) lst.get(i);
+	                 Object maxObj = lst.get(max);
+	                 if (d.compareTo(maxObj) > 0) {
+	                            max = i;
+	                 }
+	       }
+	       return max;
+	}
+
+
+}
